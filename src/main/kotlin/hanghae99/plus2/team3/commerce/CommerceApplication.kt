ccd2f04c0a84257bd0ce52b3fc02c1430a52a357
jaypark <pjhyun1990@gmail.com>
2023-09-16 23:29:49 +0900

    init project

---
+++ src/main/kotlin/hanghae99/plus2/team3/commerce/CommerceApplication.kt
@@ -0,0 +1,11 @@
+package hanghae99.plus2.team3.commerce
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CommerceApplication
+
+fun main(args: Array<String>) {
+    runApplication<CommerceApplication>(*args)
+}
---
+++ src/test/kotlin/hanghae99/plus2/team3/commerce/CommerceApplicationTests.kt
@@ -0,0 +1,13 @@
+package hanghae99.plus2.team3.commerce
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CommerceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
