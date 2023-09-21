f8f45b1db500623f4b5d202129b7dd30d14c5573
jaypark <pjhyun1990@gmail.com>
2023-09-21 15:59:54 +0900

    product project setting

---
+++ hanghae-product/src/main/kotlin/hanghae99/plus2/team3/hanghaeproduct/HanghaeOrderApplication.kt
@@ -0,0 +1,11 @@
+package hanghae99.plus2.team3.hanghaeproduct
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HanghaeOrderApplication
+
+fun main(args: Array<String>) {
+    runApplication<HanghaeOrderApplication>(*args)
+}
---
+++ hanghae-product/src/test/kotlin/hanghae99/plus2/team3/hanghaeproduct/HanghaeProductApplicationTests.kt
@@ -0,0 +1,13 @@
+package hanghae99.plus2.team3.hanghaeproduct
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HanghaeProductApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
