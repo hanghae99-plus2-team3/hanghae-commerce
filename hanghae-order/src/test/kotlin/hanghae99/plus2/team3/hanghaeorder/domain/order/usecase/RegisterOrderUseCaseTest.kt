a66ac3838c6ad07a966a880ed9a1f8657e2e2117
jaypark <pjhyun1990@gmail.com>
2023-09-21 17:44:59 +0900

    RegisterOrderUseCase order 도메인 정의

--- hanghae-order/src/test/kotlin/hanghae99/plus2/team3/hanghaeorder/domain/order/usecase/RegisterOrderUseCaseTest.kt
+++ hanghae-order/src/test/kotlin/hanghae99/plus2/team3/hanghaeorder/domain/order/usecase/RegisterOrderUseCaseTest.kt
@@ -4,6 +4,7 @@ import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import java.util.*
 
 /**
  * RegisterOrderUseCaseTest
@@ -32,12 +33,12 @@ class RegisterOrderUseCaseTest {
             receiverAddress2 = "역삼동 123-456",
             message = "부재시 경비실에 맡겨주세요",
             orderItemList = listOf(
-                RegisterOrderUseCase.Command.OrderItem(
+                RegisterOrderUseCase.Command.OrderItemCommand(
                     productId = 1L,
                     quantity = 2,
                     productPrice = 1000L,
                 ),
-                RegisterOrderUseCase.Command.OrderItem(
+                RegisterOrderUseCase.Command.OrderItemCommand(
                     productId = 2L,
                     quantity = 5,
                     productPrice = 5000L,
@@ -51,8 +52,8 @@ class RegisterOrderUseCaseTest {
 }
 
 class RegisterOrderUseCaseImpl : RegisterOrderUseCase {
-    override fun command(command: RegisterOrderUseCase.Command) : String{
-
+    override fun command(command: RegisterOrderUseCase.Command): String {
+        TODO()
     }
 
 }
@@ -60,7 +61,7 @@ class RegisterOrderUseCaseImpl : RegisterOrderUseCase {
 
 interface RegisterOrderUseCase {
 
-    fun command(command:Command) : String
+    fun command(command: Command): String
 
     data class Command(
         val userId: Long,
@@ -70,14 +71,119 @@ interface RegisterOrderUseCase {
         val receiverAddress1: String,
         val receiverAddress2: String,
         val message: String,
-        val orderItemList: List<OrderItem>,
-    ){
-        data class OrderItem(
+        val orderItemList: List<OrderItemCommand>,
+    ) {
+
+        fun toEntity(): Order {
+            return Order.create(
+                userId = userId,
+                receiverName = receiverName,
+                receiverPhone = receiverPhone,
+                receiverZipCode = receiverZipCode,
+                receiverAddress1 = receiverAddress1,
+                receiverAddress2 = receiverAddress2,
+                message = message,
+                orderItemList = orderItemList.map { it.toEntity() },
+            )
+        }
+
+        data class OrderItemCommand(
             val productId: Long,
             val quantity: Int,
             val productPrice: Long,
         )
+        {
+            fun toEntity(): OrderItem {
+                return OrderItem.create(
+                    productId = productId,
+                    quantity = quantity,
+                    productPrice = productPrice,
+                )
+            }
+        }
     }
 
 
+}
+
+data class Order private constructor(
+    val id: Long? = null,
+    val orderNum: String,
+    val userId: Long,
+    val receiverName: String,
+    val receiverPhone: String,
+    val receiverZipCode: String,
+    val receiverAddress1: String,
+    val receiverAddress2: String,
+    val message: String,
+    val orderItemList: List<OrderItem>,
+    val orderStatus: OrderStatus,
+) {
+
+    companion object{
+        fun create(
+            userId: Long,
+            receiverName: String,
+            receiverPhone: String,
+            receiverZipCode: String,
+            receiverAddress1: String,
+            receiverAddress2: String,
+            message: String,
+            orderItemList: List<OrderItem>,
+        ): Order{
+            return Order(
+                null,
+                UUID.randomUUID().toString(),
+                userId = userId,
+                receiverName = receiverName,
+                receiverPhone = receiverPhone,
+                receiverZipCode = receiverZipCode,
+                receiverAddress1 = receiverAddress1,
+                receiverAddress2 = receiverAddress2,
+                message = message,
+                orderItemList = orderItemList,
+                orderStatus = OrderStatus.ORDERED,
+            )
+        }
+
+    }
+
+    enum class OrderStatus(
+        val description: String
+    ) {
+        ORDERED("주문 완료");
+    }
+}
+
+data class OrderItem private constructor(
+    val id: Long? =null,
+    val productId: Long,
+    val quantity: Int,
+    val productPrice: Long,
+    val deliveryStatus: DeliveryStatus,
+) {
+
+    companion object{
+        fun create(
+            productId: Long,
+            quantity: Int,
+            productPrice: Long,
+        ): OrderItem{
+            return OrderItem(
+                id = null,
+                productId = productId,
+                quantity = quantity,
+                productPrice = productPrice,
+                deliveryStatus = DeliveryStatus.READY,
+            )
+        }
+    }
+
+    enum class DeliveryStatus(
+        val description: String
+    ) {
+        READY("배송 준비중"),
+        DELIVERY("배송 중"),
+        COMPLETE("배송 완료");
+    }
 }
