604f97bc2f8a41e2362431c39348a7470e793e03
jaypark <pjhyun1990@gmail.com>
2023-09-24 23:51:54 +0900

    OrderPaymentUseCass 실패하는 테스트 추가. (주문 생성자와 결제 요청자가 다름)

--- hanghae-order/src/test/kotlin/hanghae99/plus2/team3/hanghaeorder/domain/order/small/OrderPaymentUseCaseTest.kt
+++ hanghae-order/src/test/kotlin/hanghae99/plus2/team3/hanghaeorder/domain/order/small/OrderPaymentUseCaseTest.kt
@@ -4,10 +4,13 @@ import hanghae99.plus2.team3.hanghaeorder.domain.order.DeliveryInfo
 import hanghae99.plus2.team3.hanghaeorder.domain.order.Order
 import hanghae99.plus2.team3.hanghaeorder.domain.order.infrastructure.OrderItemRepository
 import hanghae99.plus2.team3.hanghaeorder.domain.order.infrastructure.OrderRepository
+import hanghae99.plus2.team3.hanghaeorder.domain.order.infrastructure.QueryUserInfoByApi
 import hanghae99.plus2.team3.hanghaeorder.domain.order.mock.FakeOrderRepositoryImpl
+import hanghae99.plus2.team3.hanghaeorder.domain.order.mock.FakeQueryUserInfoByApiImpl
 import hanghae99.plus2.team3.hanghaeorder.domain.order.usecase.RegisterOrderUseCase
 import hanghae99.plus2.team3.hanghaeorder.exception.ErrorCode
 import hanghae99.plus2.team3.hanghaeorder.exception.OrderNotFoundException
+import hanghae99.plus2.team3.hanghaeorder.exception.OrderedUserNotFoundException
 import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
@@ -25,8 +28,15 @@ class OrderPaymentUseCaseTest {
 
     @BeforeEach
     fun setUp() {
+        val users = listOf(
+            QueryUserInfoByApi.UserInfo(
+                userId = 1L,
+                userName = "홍길동",
+                userEmail = "test@gmail.com"
+            )
+        )
         val orderRepository = FakeOrderRepositoryImpl()
-        sut = OrderPaymentUseCaseImpl(orderRepository)
+        sut = OrderPaymentUseCaseImpl(orderRepository, FakeQueryUserInfoByApiImpl(users))
 
         orderRepository.save(
             Order(
@@ -73,6 +83,21 @@ class OrderPaymentUseCaseTest {
         }.isInstanceOf(OrderNotFoundException::class.java)
             .hasMessage(ErrorCode.ORDER_NOT_FOUND.message)
     }
+
+    @Test
+    fun `주문자의 정보와 결제 요청자의 정보가 다를 때 결제하면 기대하는 응답(실패)을 반환한다`() {
+        assertThatThrownBy {
+            sut.command(
+                OrderPaymentUseCase.Command(
+                    orderId = 1L,
+                    userId = 2L,
+                    paymentType = PaymentType.CARD,
+                    paymentAmount = 5000L,
+                )
+            )
+        }.isInstanceOf(OrderNotFoundException::class.java)
+            .hasMessage(ErrorCode.ORDER_INFO_NOT_VALID.message)
+    }
 }
 
 interface OrderPaymentUseCase {
@@ -92,6 +117,7 @@ enum class PaymentType {
 
 class OrderPaymentUseCaseImpl(
     private val orderRepository: OrderRepository,
+    private val queryUserInfoByApi: QueryUserInfoByApi,
 ) : OrderPaymentUseCase {
 
     companion object {
@@ -99,6 +125,9 @@ class OrderPaymentUseCaseImpl(
     }
 
     override fun command(command: OrderPaymentUseCase.Command): String {
+        if (queryUserInfoByApi.query(command.userId) == null)
+            throw OrderedUserNotFoundException()
+
         val order = (orderRepository.findByIdOrNull(command.orderId)
             ?: throw OrderNotFoundException())
         return PAYMENT_PREFIX + command.orderId
