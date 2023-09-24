a1bc80718a34806c3bffa3b4fef29c987a96a668
jaypark <pjhyun1990@gmail.com>
2023-09-24 23:17:16 +0900

    OrderPaymentUseCase (정상적인 주문 요청) 테스트 통과

--- hanghae-order/src/test/kotlin/hanghae99/plus2/team3/hanghaeorder/domain/order/small/OrderPaymentUseCaseTest.kt
+++ hanghae-order/src/test/kotlin/hanghae99/plus2/team3/hanghaeorder/domain/order/small/OrderPaymentUseCaseTest.kt
@@ -1,8 +1,11 @@
 package hanghae99.plus2.team3.hanghaeorder.domain.order.small
 
+import hanghae99.plus2.team3.hanghaeorder.domain.order.infrastructure.OrderItemRepository
+import hanghae99.plus2.team3.hanghaeorder.domain.order.infrastructure.OrderRepository
 import hanghae99.plus2.team3.hanghaeorder.domain.order.usecase.RegisterOrderUseCase
 import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 /**
@@ -13,21 +16,52 @@ import org.junit.jupiter.api.Test
  * @since 2023/09/24
  */
 class OrderPaymentUseCaseTest {
+    private lateinit var sut: OrderPaymentUseCase
+
+    @BeforeEach
+    fun setUp() {
+        sut = OrderPaymentUseCaseImpl()
+    }
 
     @Test
     fun `정상적으로 주문한 내역의 결제 요청을 하면 기대하는 응답(성공)을 반환한다`() {
 
-        val paymentId = OrderPaymentUseCase.command(
-            Command(
+        val paymentId = sut.command(
+            OrderPaymentUseCase.Command(
                 orderId = 1L,
                 userId = 1L,
                 paymentType = PaymentType.CARD,
                 paymentAmount = 5000L,
             )
         )
-
         assertThat(paymentId).isNotNull
     }
 }
 
+interface OrderPaymentUseCase{
+    fun command(command: Command): String
 
+    data class Command(
+        val orderId: Long,
+        val userId: Long,
+        val paymentType: PaymentType,
+        val paymentAmount: Long,
+    )
+}
+
+enum class PaymentType {
+    CARD,
+}
+
+class OrderPaymentUseCaseImpl(
+) : OrderPaymentUseCase {
+
+    companion object {
+        private const val PAYMENT_PREFIX = "PAYMENT"
+    }
+
+    override fun command(command: OrderPaymentUseCase.Command): String {
+
+        return PAYMENT_PREFIX + command.orderId
+    }
+}
