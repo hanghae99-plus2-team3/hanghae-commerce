2eaef94564dfdc9d7d01723d7192b04dab476a37
jaypark <pjhyun1990@gmail.com>
2023-09-27 22:42:33 +0900

    도메인 모듈 변경사항 반영

---
+++ hanghae-auth-user/src/main/kotlin/hanghae99/plus2/team3/hanghaeauthuser/HanghaeAuthUserApplication.kt
@@ -0,0 +1,11 @@
+package hanghae99.plus2.team3.hanghaeauthuser
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HanghaeAuthUserApplication
+
+fun main(args: Array<String>) {
+    runApplication<HanghaeAuthUserApplication>(*args)
+}
---
+++ hanghae-auth-user/src/test/kotlin/hanghae99/plus2/team3/hanghaeauthuser/HanghaeAuthUserApplicationTests.kt
@@ -0,0 +1,13 @@
+package hanghae99.plus2.team3.hanghaeauthuser
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HanghaeAuthUserApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
