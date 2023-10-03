4adb743f639ac10f5b9338c2383ff4cd1b39ceda
jaypark <pjhyun1990@gmail.com>
2023-10-03 23:57:08 +0900

    test : 존재하지 않는 주문 취소 테스트 케이스 추가

--- hanghae-order/src/test/kotlin/hanghae99/plus2/team3/hanghaeorder/domain/order/small/CancelOrderUseCaseTest.kt
+++ hanghae-order/src/test/kotlin/hanghae99/plus2/team3/hanghaeorder/domain/order/small/CancelOrderUseCaseTest.kt
@@ -1,6 +1,9 @@
 package hanghae99.plus2.team3.hanghaeorder.domain.order.small
 
+import hanghae99.plus2.team3.hanghaeorder.common.exception.ErrorCode
+import hanghae99.plus2.team3.hanghaeorder.common.exception.OrderNotFoundException
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
@@ -31,6 +34,21 @@ class CancelOrderUseCaseTest {
             )
         )
         assertThat(result).isEqualTo(orderNum)
+    }
+
+    @Test
+    fun `존재 하지 않는 주문을 취소하면 기대하는 응답(실패)을 반환한다`() {
+        val orderNum = "orderNum-99"
+        val userId = 1L
+        assertThatThrownBy {
+            sut.command(
+                CancelOrderUseCase.Command(
+                    orderNum,
+                    userId,
+                )
+            )
+        }.isInstanceOf(OrderNotFoundException::class.java)
+            .hasMessage(ErrorCode.ORDER_NOT_FOUND.message)
     }
 
 }
