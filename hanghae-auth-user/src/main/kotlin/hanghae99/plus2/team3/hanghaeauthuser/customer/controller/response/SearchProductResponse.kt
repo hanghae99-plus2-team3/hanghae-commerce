82fa9235651b452478626b3a5c22316cdce60d33
jungcali <jungcali94@gmail.com>
2023-10-12 00:41:35 +0900

    상품 검색 테스트

---
+++ hanghae-auth-user/src/main/kotlin/hanghae99/plus2/team3/hanghaeauthuser/customer/controller/response/SearchProductResponse.kt
@@ -0,0 +1,5 @@
+package hanghae99.plus2.team3.hanghaeauthuser.customer.controller.response
+
+data class SearchProductResponse(
+    val name: String
+)
---
+++ hanghae-auth-user/src/main/kotlin/hanghae99/plus2/team3/hanghaeauthuser/customer/infrastructure/ProductAccessor.kt
@@ -0,0 +1,4 @@
+package hanghae99.plus2.team3.hanghaeauthuser.customer.infrastructure
+
+interface ProductAccessor {
+}
