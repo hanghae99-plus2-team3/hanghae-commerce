e42fe693e58d98942084464059ff15364ce901b7
jaypark <pjhyun1990@gmail.com>
2023-10-19 21:29:20 +0900

    feat: file append 추가

--- hanghae-common/build.gradle.kts
+++ hanghae-common/build.gradle.kts
@@ -1,6 +1,15 @@
+dependencies {
+
+    // 로그백 의존성
+    implementation(group = "ca.pjer", name = "logback-awslogs-appender", version = "1.6.0")
+    // 프로퍼티 제어 in xml
+    implementation("org.codehaus.janino:janino:3.1.7")
+}
+
 tasks.bootJar {
     enabled = false
 }
 tasks.jar {
     enabled = true
 }
+
